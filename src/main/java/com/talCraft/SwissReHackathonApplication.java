0f60a41c719c6a7bc92f500ca5a6f4947cd502fd
Ashish Waghmare <ashish.waghmare@talentica.com>
2016-01-23 19:03:34 +0530

    Staticc pages to start with

---
+++ src/main/java/com/talCraft/SwissReHackathonApplication.java
@@ -0,0 +1,14 @@
+package com.talCraft;
+
+import com.ibm.watson.developer_cloud.tradeoff_analytics.v1.TradeoffAnalytics;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissReHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwissReHackathonApplication.class, args);
+		TradeoffAnalytics analytics;
+	}
+}
