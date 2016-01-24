3659644a6176cba8c549ae7a1aa2dd09a4899339
Swapnil Bhagat <swapnil.bhagat@talentica.com>
2016-01-24 16:49:02 +0530

    Sample jsf demo code

---
+++ jsf-demo/src/main/java/com/jsf/demo/DemoModel.java
@@ -0,0 +1,51 @@
+package com.jsf.demo;
+
+public class DemoModel {
+
+	private String firstName;
+	private String lastName;
+	private Integer weightInKg;
+	private Integer heightInFeet;
+	private Integer heightInInches;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getWeightInKg() {
+		return weightInKg;
+	}
+
+	public void setWeightInKg(Integer weightInKg) {
+		this.weightInKg = weightInKg;
+	}
+
+	public Integer getHeightInFeet() {
+		return heightInFeet;
+	}
+
+	public void setHeightInFeet(Integer heightInFeet) {
+		this.heightInFeet = heightInFeet;
+	}
+
+	public Integer getHeightInInches() {
+		return heightInInches;
+	}
+
+	public void setHeightInInches(Integer heightInInches) {
+		this.heightInInches = heightInInches;
+	}
+
+}
